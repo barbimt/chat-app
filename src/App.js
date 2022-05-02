07399f06c20a2479e92de9a9fe0b2742dec54ed7
barbimt <82407633+barbimt@users.noreply.github.com>
2022-05-02 15:32:02 -0300

    add files for chat App in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed.jsx';
+import LoginForm from './components/LoginForm';
+
 
 function App() {
+  if(!localStorage.getItem('username')) return <LoginForm />
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ChatEngine 
+      height="100vh"
+      projectID="0d5168d2-784a-47d9-bfbc-37b0efd8248a"
+      userName="barbi"
+      userSecret="123"
+      renderChatFeed= {(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage = {() =>new Audio('https://chat-engine-assets.s3.amazonws.com/click.mp3').play}
+    />
     </div>
   );
 }
